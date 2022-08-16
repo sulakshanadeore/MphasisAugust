da2f7174b7d51e68ffc10815151f84fd019c8e5c
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-16 13:01:41 +0530

    Second Program

--- Program.cs
+++ Program.cs
@@ -7,23 +7,36 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int i=10;
-            Int32 j=20;
-            int k = i + j;
-            Console.WriteLine(k);
-            Console.WriteLine("--------------------------");
-            Console.WriteLine("enter i for multiplication");
-            i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter j for multiplication");
-            j = Convert.ToInt32(Console.ReadLine());
-            k = i * j;
-
-            Console.WriteLine("The multiplication of i and j=" +  k);
-            Console.WriteLine("The multiplication of {0} and {1}={2}",i,j,k );
-               
-
-            Console.WriteLine("Hello");
-            Console.Write(DateTime.Now);
+            int i=0,j=0,k=0,userchoice;
+            char ch='+';
+            Console.WriteLine("Menu \n 1.Add \n 2.Subtract \n 3.Exit");
+            userchoice = Convert.ToInt32(Console.ReadLine());
+            if (userchoice!=3)
+            {
+                Console.WriteLine("Enter value for i");
+                i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter value for j");
+                j = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            switch (userchoice)
+            {
+                case 1:
+                    ch = '+';
+                    k = i + j;
+                    break;
+                case 2:
+                    ch = '-';
+                    k = i - j;
+                    break;
+                case 3:
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Environment.Exit(1);
+                    break;
+            }
+            Console.WriteLine("The answer of {0} {1} {2}={3}",i,ch,j,k);
 
             Console.ReadLine();//Read()/ ReadKey()
             //comment
